4b4457398a8a4397feaab06d6e6e4ac8040fd86a
yukimura1227 <takamura1227@gmail.com>
2018-07-30 01:04:40 +0900

    skelton

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const parseJSON = require('json-parse-async');
+const puppeteer = require('puppeteer');
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const electron = require("electron");
+const {app} = electron;
+
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
+
+async function take_screen_shot(url, filename) {
+  const page = await global.browser.newPage();
+  page.setViewport({width: 1200, height: 800})
+
+  await page.goto(url);
+  await page.waitForNavigation({waitUntil:'networkidle2', timeout: 30000})
+            .catch(e => console.log('timeout exceed. proceed to next operation'))
+  return page.screenshot({path: global.result_dir + '/' + filename, fullPage: true});
+};
+
+async function diff(filename) {
+  var result_file_path = global.result_dir + '/' + filename;
+  var origin_file_path = app.getPath('userData') + '/origin/' + filename;
+  if(!fs.existsSync(result_file_path) || !fs.existsSync(origin_file_path)) {
+    return;// new Promise((resolve, reject) => {resolve();});
+  }
+  var img1 = await PNG.sync.read(fs.readFileSync(result_file_path));
+  var img2 = await PNG.sync.read(fs.readFileSync(origin_file_path));
+  var diff = await new PNG({width: img1.width, height: img1.height});
+  await pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1});
+  return diff.pack().pipe(fs.createWriteStream(app.getPath('userData') + '/diff/' + filename));
+}
+
+module.exports.take_all_screen_shot = take_all_screen_shot;
+function take_all_screen_shot() {
+  (async () => {
+    var date = new Date();
+    global.result_dir = app.getPath('userData') + '/test_result/' + date.getTime();
+    fs.mkdirSync(global.result_dir);
+    global.browser = await puppeteer.launch();
+
+    parseJSON(fs.readFileSync(app.getPath('userData') + '/url_list.json', 'utf8'))
+      .then(async (content) => {
+        var url_info_list = content.url_info_list
+
+        await Promise.all( url_info_list.map(async (url_info) => {
+          await take_screen_shot(url_info.url, url_info.filename);
+        }));
+
+        await global.browser.close();
+
+        await imagemin([global.result_dir+ '/*.png'], global.result_dir + '/', {
+          plugins: [
+            imageminPngquant()
+          ]
+        });
+
+        await Promise.all( url_info_list.map(async (url_info) => {
+          diff(url_info.filename);
+        }));
+
+        console.log('all done');
+      })
+      .catch(function (err) {
+         console.log('promise was rejected:', err);
+      });
+  })();
+}
---
+++ main.js
@@ -0,0 +1,79 @@
+"use strict";
+const electron        = require("electron");
+const {app, BrowserWindow, ipcMain, shell} = electron;
+const fse = require('fs-extra')
+initialize_env();
+const screen_shot_util = require('./index.js');
+
+global.mainWindow = null;
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  electron.session.defaultSession.clearCache(() => {});
+  app.quit();
+});
+
+app.on('activate', () => {
+  createWindow();
+});
+
+function createWindow() {
+  if(global.mainWindow !== null ) {
+    return;
+  }
+  global.mainWindow = new BrowserWindow({width: 350, height: 600});
+
+  global.mainWindow.loadURL('file://' + __dirname + '/index.html');
+  global.mainWindow.webContents.session.clearCache(function() {});
+
+  // global.mainWindow.webContents.openDevTools();
+
+  // ウィンドウが閉じられたらアプリも終了
+  global.mainWindow.on('closed', () => global.mainWindow = null );
+}
+
+ipcMain.on('take_screen_shots', (event, arg) => {
+  screen_shot_util.take_all_screen_shot();
+});
+
+ipcMain.on('open_origin_directory', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/' + 'origin' + '/');
+});
+
+ipcMain.on('open_url_list', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/url_list.json');
+});
+
+ipcMain.on('open_diff_directory', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/' + 'diff' + '/');
+});
+
+ipcMain.on('open_test_result_directory', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/' + 'test_result' + '/');
+});
+
+ipcMain.on('open_origin_directory', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/' + 'origin' + '/');
+});
+
+function initialize_env() {
+  var copy_targets = ['url_list.json'];
+  copy_targets.forEach( (filename) => {
+    console.log(filename)
+    var copy_from = __dirname + '/' + filename;
+    var copy_dist = app.getPath('userData') + '/' + filename;
+    if(!fse.existsSync(copy_dist)) {
+      fse.copySync(copy_from, copy_dist);
+    }
+  });
+  var base_directories = ['diff', 'origin', 'test_result'];
+  base_directories.forEach( (dirname) => {
+    var directory_path = app.getPath('userData') + '/' + dirname
+    if(!fse.existsSync(directory_path)) {
+      fse.mkdirSync(directory_path);
+    }
+  });
+}
